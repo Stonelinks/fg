3f41efcdd87c705716404b25d171bf092d500a40
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-11-29 15:21:01 -0500

    title editing

--- main.js
+++ main.js
@@ -1,52 +1,102 @@
 $(document).ready(function() {
 
+  var inspect = function(thing) {
+    console.log(JSON.stringify(thing, null, 4));
+  };
+
   var URLtoData = function(hash) {
-    return JSON.parse(atob(decodeURIComponent(hash.slice(1))))
-  }
-  
+    return hash ? JSON.parse(atob(decodeURIComponent(hash.slice(1)))) : {};
+  };
+
   var dataToURL = function(data) {
-    return '#' + encodeURIComponent(btoa(JSON.stringify(data)))
-  }
+    return '#' + encodeURIComponent(btoa(JSON.stringify(data)));
+  };
 
   var DataModel = Backbone.Model.extend({
     defaults: {
-      text: 'hey'
+      title: 'Double click to edit title'
     },
-    
+
     initialize: function() {
       this.on('change', function() {
-        window.location.hash = dataToURL(this.toJSON())
-      })
+        window.location.hash = dataToURL(this.toJSON());
+      });
     }
   });
 
-  var DataView = Marionette.ItemView.extend({
+  var TitleView = Marionette.ItemView.extend({
 
-    template: '#text-template',
-    
+    template: '#title-template',
+
     modelEvents: {
       'change': 'render'
-    }
+    },
 
-  })
+    ui: {
+      edit: '.edit',
+      label: '.title-label'
+    },
+
+    events: {
+      'dblclick @ui.label': 'onEditClick',
+      'keydown @ui.edit': 'onEditKeypress',
+      'focusout @ui.edit': 'onEditFocusout'
+    },
+
+    onEditClick: function() {
+      this.$el.addClass('editing');
+      this.ui.edit.focus();
+      this.ui.edit.val(this.ui.edit.val());
+    },
+
+    onEditFocusout: function() {
+      var todoText = this.ui.edit.val().trim();
+      if (todoText) {
+        this.model.set('title', todoText);
+      }
+      else {
+        this.model.set('title', DataModel.prototype.defaults.title);;
+      }
+      this.$el.removeClass('editing');
+    },
+
+    onEditKeypress: function(e) {
+      var ENTER_KEY = 13;
+      var ESC_KEY = 27;
+
+      if (e.which === ENTER_KEY) {
+        this.onEditFocusout();
+        return;
+      }
+
+      if (e.which === ESC_KEY) {
+        this.ui.edit.val(this.model.get('title'));
+        this.$el.removeClass('editing');
+      }
+    }
+  });
 
   var myApp = new Backbone.Marionette.Application();
 
   myApp.addInitializer(function(options) {
 
     myApp.addRegions({
-      textAnchor: '#text-anchor'
+      titleAnchor: '#title-anchor',
+      graphAnchor: '#graph-anchor',
+      controlsAnchor: '#controls-anchor'
     });
-
-    var data = new DataModel(URLtoData(window.location.hash));
     
-    window.data = data
+    var urlData = URLtoData(window.location.hash)
+    inspect(urlData)
 
-    var view = new DataView({
+    var data = new DataModel(urlData);
+
+    window.data = data;
+
+    var titleView = new TitleView({
       model: data
-    })
-
-    myApp.textAnchor.show(view);
+    });
+    myApp.titleAnchor.show(titleView);
 
   });
 
